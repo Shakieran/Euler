8db7ddda7e1f6b8ed40f27356cc0c1af6e15bb89
Shakieran <kiedraney@gmail.com>
2016-05-26 14:28:54 -0400

    Create SeventyTwo.java
    Adding this so I can access it and dsutff

---
+++ SeventyTwo.java
@@ -0,0 +1,16 @@
+import java.math.*;
+import java.util.*;
+
+public class SeventyTwo
+{
+  private ArrayList<Integer> primes = Basic.sievePrimes(500000);
+  private BigInteger sum = new BigInteger("0");
+  
+  public SeventyTwo()
+  {
+    for(int x = 0; x < primes.size(); x++)
+    {
+      
+    }
+  }
+}
