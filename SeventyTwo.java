25be4d8b208eb6989722904ac9c04261bea57c2c
Shakieran <kiedraney@gmail.com>
2016-05-27 09:39:03 -0400

    Update SeventyTwo.java
    Fixed small bugs

--- SeventyTwo.java
+++ SeventyTwo.java
@@ -4,31 +4,31 @@ import java.util.*;
 public class SeventyTwo
 {
   private ArrayList<Integer> primes = Basic.sievePrimes(500000);
-  private BigInteger sum = new BigInteger("0");
-  
+  private BigInteger Sum = new BigInteger("0");
+
   public SeventyTwo()
   {
     int c = 0;
     int d;
     ArrayList<Integer> phi = new ArrayList<Integer>();
     phi.add(2);
-    
-    c = primes.get(x);
+
+    c = primes.get(0);
     while(c < 1000000)
        c *= 2;
-      
+
     c /= 2;
-     
+
      while(c > -1)
      {
-       d = (int)(2, c));
+      d = (int)Math.pow(2, c);
       getNums(0, d, phi);//primeNum pos, the number it forms, and the numbers making it(for phi of n)
       c--;
     }//Do we need c to be sent? -- no
-    
-    System.out.println(sum.toString());
+
+    System.out.println(Sum.toString());
   }
-  
+
   private void getNums(int x, int d, ArrayList<Integer> phi)
   {
     int c = 0;
@@ -37,9 +37,9 @@ public class SeventyTwo
     {
       if(d*primes.get(x)>1000000)
       {
-        for(int x = 0; x < phi.size(); x++)
-          d *= (1-(1.0/phi.get(x)));
-          
+        for(int f = 0; f < phi.size(); f++)
+          d *= (1-(1.0/phi.get(f)));
+
         Sum.add(new BigInteger("" + d));
       }
       else
@@ -50,15 +50,15 @@ public class SeventyTwo
           c++;
           d *= primes.get(x);
         }
-        
+
         c--;
         d /= primes.get(x);
-        
+
         while(c > 0)
         {
           getNums(x, d, phi);
           c--;
-          
+
         }
       }
     }
@@ -67,7 +67,7 @@ public class SeventyTwo
       double jenky = 1;
       for(int g = 0; g < phi.size(); g++)
         jenky *= (1 - 1.0/phi.get(g));
-        
+
       Sum.add(new BigInteger("" + (int)(d * jenky)));
     }
   }
