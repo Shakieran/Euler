b76a0fc9d5b432f5e86dbf3bb5315a403efce3b9
Shakieran <kiedraney@gmail.com>
2016-09-08 14:57:08 -0400

    Update ThirtySeven.java

--- ThirtySeven.java
+++ ThirtySeven.java
@@ -1,4 +1,92 @@
-import java.math.*;
+//FINAL ANSWER
+public class HelloWorld{
+   
+    public HelloWorld()
+  {
+    int sum = 0;
+    int total = 0;
+    int x = 11;//2, 3, 5, 7, 11
+    while(total < 11)
+    {
+       try
+       {
+      		if(isPrime(x) && rightTrunc(x) && leftTrunc(x))
+     	  	{
+     	  	  sum += x;
+     	  	  total++;
+       	 	 System.out.println(x);
+       		}
+       }
+       catch(IndexOutOfBoundsException e)
+       {
+       		System.out.println(sum + ":" + total);
+       		total = 12;
+       }
+    	x+=2;
+    }
+    System.out.println("Sum: " + sum);
+  }
+    
+  private boolean rightTrunc(int num)//We assume that the number is prime
+  {
+    int ten = 10;
+    if(!isPrime(num % 10))
+      return false;
+    while(ten*10 < num)
+      ten *= 10;
+    while(num > 10)
+    {
+      num = num % ten;// 327 % 100 = 27
+      if(!isPrime(num))
+        return false;
+      ten/=10;
+    }
+    return true;
+  }
+  private boolean leftTrunc(int num)
+  {
+    //(num-(num % 10))/10
+    while(num > 1)
+    {
+      num = (num - (num % 10))/10;
+      if(!isPrime(num))
+        return false;
+    }
+    return true;
+  }
+  
+  public static boolean isPrime(int n)
+  {
+      if(n==1)
+        return false;
+      
+      int x = 1;
+      
+      while((x+1)*(x+1) <= n)
+      {
+          x++;
+      }
+      
+      while(x>1)
+      {
+          if(n%x==0)
+            return false;
+            
+        x--;
+      }
+      
+      return true;
+  }
+    
+     public static void main(String []args){
+        HelloWorld temp = new HelloWorld(); //+ ", " + rightTrunc(23));
+     }
+}
+
+
+
+
+/*import java.math.*;
 import java.util.*;
 //23
 public class ThirtySeven
